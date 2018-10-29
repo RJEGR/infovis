94d0033e639556b153f5ded9123c10b4f9276f74
cicese <cigom@ciceses-MacBook-Pro.local>
2018-10-29 14:28:13 -0700

    monday
    ok

--- subsample.py
+++ subsample.py
@@ -8,10 +8,12 @@
 """
     This file contains code than helps to wragling fasta/fastq files. \
     The code was developed in python 3.6+. \
-    By default a set of 100 sequence is sampled and output_name_file gonna be the same than input but using the "subset" prefix, \
+    By default a set of N sequence is sampled and output_name_file gonna be the same than input but using the "subset" prefix, \
     This current version output an standard-format in FASTA''',
     to use, two argument needed: sequence file (fasta/fastq format) and sequence format(fastq/fastq).\
-     Ex: subsample.py input.fasta fasta 
+     Ex: ./subsamples.py input.fasta file N
+	./subsample.py input.fasta fasta 100
+
 """
 
 from Bio import SeqIO
@@ -24,13 +26,15 @@ file = sys.argv[1]
 #out = open(sys.argv[2], 'w')
 out = open(file+'.subset', 'w')
 frt = sys.argv[2]
+v = sys.argv[3]
+size = int(v)
 sys.stdout = out
 orig_stdout = sys.stdout
 
 
 with open(file) as f:
     seqs = SeqIO.parse(f, frt)
-    subset = ((seq.name, seq.seq) for seq in  sample(list(seqs),100))
+    subset = ((seq.name, seq.seq) for seq in  sample(list(seqs), size))
     for i in subset:
         print(">{}\n{}".format(*i))
 
