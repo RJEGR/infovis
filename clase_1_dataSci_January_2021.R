18dfeb18c3d0e747bc810461d7a981bb76f15112
Ricardo Gore <rjhgore@gmail.com>
2021-01-08 08:25:09 -0800

    Update clase_1_dataSci_January_2021.R

--- clase_1_dataSci_January_2021.R
+++ clase_1_dataSci_January_2021.R
@@ -39,13 +39,13 @@ names(obj) # tambien se puede usar colnames
 head(obj)
 tail(obj)
 str(obj)
-View(obj)
+# View(obj)
 
 # # # # # #
 # ordenar tabla de datos ----
 # # # # # #
 
-# start beepr::beep(3)
+# start beepr::beep(8)
 
 # # # # # # # # # #
 # Cargamos programas ----
@@ -54,33 +54,107 @@ View(obj)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+
 # # # # # # #
 # uso de tuberias
 # # # # # # #
 
 obj %>% head()
+obj %>% tibble::glimpse()
 
+# re-organizando datos:
 
+obj <- obj %>% as_tibble()
 
-# previs
-
-
-
-
-obj %>% 
-  as_tibble() %>%
-  # select_if(Negate(is.double)) %>%
-  select(Samples_Name,  Sampling_Area, Replicate )
-
-ranks <- c('Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
+# Metodos de seleccion de columnas
 
 obj %>% 
-  as_tibble() %>%
+  select(index, Samples_Name,  Sampling_Area, Replicate) %>% 
+  View()
+
+obj %>% 
   select_if(is.double)  %>% 
-  select(-Sampling_Date) %>%
-  t() %>% as_tibble(rownames = 'linaje') %>% 
-  separate(col = linaje, into = ranks, sep = '[.]') %>%
-  mutate_all(., funs(str_replace_all(., c("^D_[0-9]__"="")))) 
+  View()
+
+obj %>% 
+  select_at(vars(contains("D_0"))) %>%
+  View()
+
+# 
+
+library(stringr)
+
+colNames <- obj %>% select_at(vars(contains("D_0"))) %>% names()
+
+obj %>% 
+  pivot_longer(cols = colNames, 
+               names_to = "linaje", 
+               values_to = "ab") -> obj_longer 
+
+obj_longer %>% View()
+
+# datavis
+# 1.
+obj_longer %>%
+  ggplot(aes(x = linaje, y = ab)) +
+  geom_col() +
+  coord_flip()
+
+# 2.
+obj_longer %>%
+  ggplot(aes(x = linaje, y = ab, fill = Sampling_Area)) +
+  geom_col() +
+  coord_flip()
 
 # Expresiones regulares
+ranks <- c('Kingdom', 'Phylum', 'Class', 'Order', 'Family')
 
+obj_longer %>% 
+  separate(col = linaje, into = ranks, sep = '[.]') %>% 
+  mutate_at(ranks, funs(str_replace_all(., c("^D_[0-9]__"=""))))  -> obj_longer
+
+obj_longer %>%
+  separate(col = Replicate, into = c('Sample','Rep'), sep = '_') -> obj_longer
+  
+obj_longer %>% tibble::glimpse()
+
+# dataVis
+
+obj_longer %>%
+  ggplot(aes(x = Sampling_Area, y = ab, fill = Phylum)) +
+  coord_flip() +
+  geom_bar(stat = "identity", 
+           position = "stack", color = "black")
+
+raT <- function(x) {(x / sum (x) ) * 100}
+
+obj_longer %>%
+  group_by(index) %>%
+  mutate(RA = raT(ab)) %>%
+  filter(Rep %in% "1") %>%
+  ggplot(aes(x = Sampling_Area, y = RA, fill = Phylum)) +
+  coord_flip() +
+  geom_bar(stat = "identity", 
+           position = "stack", color = "black") -> plot
+
+# Aesthetic
+
+plot + ggsci::scale_fill_aaas()
+plot + ggsci::scale_fill_cosmic()
+plot + ggsci::scale_fill_rickandmorty()
+
+plot + ggsci::scale_fill_rickandmorty() +
+  theme_classic(base_size = 14)
+
+# facet
+
+obj_longer %>%
+  group_by(index) %>%
+  mutate(RA = raT(ab)) %>%
+  ggplot(aes(x = index, y = RA, fill = Phylum)) +
+  coord_flip() + labs(x = 'Replicate', y = 'Rel. Ab (%)') +
+  geom_bar(stat = "identity", 
+           position = "stack", color = "black") +
+  facet_grid( Sampling_Area ~., scales = "free_y") +
+  ggsci::scale_fill_rickandmorty() +
+  theme_classic(base_size = 14)
