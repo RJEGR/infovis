694f3dfd703d92540034a6cf948acec752cba443
Ricardo Gore <rjhgore@gmail.com>
2021-01-09 14:08:25 -0800

    Update clase_1_dataSci_January_2021.R

--- clase_1_dataSci_January_2021.R
+++ clase_1_dataSci_January_2021.R
@@ -158,3 +158,48 @@ obj_longer %>%
   facet_grid( Sampling_Area ~., scales = "free_y") +
   ggsci::scale_fill_rickandmorty() +
   theme_classic(base_size = 14)
+
+# create my own method
+
+my_tidy_function <- function(file) {
+  
+  obj <- read.csv(file)
+  
+  colNames <- obj %>% select_at(vars(contains("D_0"))) %>% names()
+  
+  obj %>% pivot_longer(cols = colNames, 
+                 names_to = "linaje", 
+                 values_to = "ab") %>%
+    mutate(fileName = basename(file)) %>%
+    select(fileName, Replicate, index, linaje, ab) %>%
+    mutate(fileName = str_replace_all(fileName, "_level-5.csv", ""))
+  
+  
+  
+}
+
+my_tidy_function(files[1])
+
+# Concatenate tidy 
+
+obj_longer <- lapply(files, my_tidy_function) %>% do.call(rbind, .)
+obj_longer %>% tibble::glimpse()
+
+obj_longer %>% 
+  separate(col = linaje, into = ranks, sep = '[.]') %>% 
+  mutate_at(ranks, funs(str_replace_all(., c("^D_[0-9]__"="")))) %>%
+  separate(col = Replicate, into = c('Sample','Rep'), sep = '_') -> obj_longer
+
+obj_longer %>% View()
+
+obj_longer %>%
+  filter(Rep %in% "1") %>%
+  group_by(index, fileName) %>%
+  mutate(RA = raT(ab)) %>%
+  ggplot(aes(x = fileName, y = RA, fill = Phylum)) +
+  coord_flip() + labs(x = 'Amplicon', y = 'Rel. Ab (%)') +
+  geom_bar(stat = "identity", 
+           position = "stack", color = "black") +
+  facet_grid( Sample ~., scales = "free_y") +
+  ggsci::scale_fill_rickandmorty() +
+  theme_classic(base_size = 14)
