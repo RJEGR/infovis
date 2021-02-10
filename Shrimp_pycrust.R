6c7ab76e20adfb4c2f309473b440b3a48d1d9940
Ricardo Gore <rjhgore@gmail.com>
2021-02-09 19:15:37 -0800

    Update Shrimp_pycrust.R

--- Shrimp_pycrust.R
+++ Shrimp_pycrust.R
@@ -7,7 +7,8 @@ files <- list.files(path = dir, pattern = ".xlsx$", full.names = TRUE)
 library(readxl)
 library(tidyverse)
 library(ggh4x)
-
+library(pheatmap)
+# install_github("raivokolde/pheatmap")
 
 obj <- read_xlsx(files[1])
 mtd_file <- list.files(path = dir, pattern = "CIAD_MappingFile.txt$", full.names = TRUE)
@@ -58,6 +59,14 @@ LCategory <- dataViz %>% group_by(Category) %>% summarise(t = sum(RA)) %>% arran
 
 library(ggsci)
 
+labels <- dataViz %>% pull(Category) %>% unique() %>% sort()
+fillCol <- length(labels)
+
+if(fillCol > 26) {
+  getPalette <- colorRampPalette(pal_ucscgb()(26))(fillCol)
+} else
+  getPalette <- pal_ucscgb()(fillCol)
+
 dataViz %>%
   mutate(Category = factor(Category, 
                                levels = LCategory)) %>%
@@ -66,7 +75,7 @@ dataViz %>%
   geom_col(aes(x = SuperPathway ,
                y = RA, fill = Category)) +
   labs(y = "Relative Abundance of predicted gene (%)") +
-  ggsci::scale_fill_ucscgb() +
+  scale_fill_manual(values = getPalette) +
   facet_grid(Category ~ Tissue , scales = "free", space = "free_y") +
   coord_flip() +
   theme_classic(base_size = 16, base_family = "GillSans") +
@@ -76,3 +85,51 @@ dataViz %>%
 
 ggsave(barPlot, filename = "barKegg.png", path = dir, 
        width = 18, height = 10)
+
+# pheatmap ----
+# en base a las 33 categorias de L2 ie. obj %>% group_by(SuperPathway)
+
+obj %>% select_at(vars(!colNames)) %>% distinct(SuperPathway, .keep_all = T) -> left_j
+
+obj %>% 
+  group_by(SuperPathway) %>%
+  summarise_at(vars(colNames), sum) %>%
+  left_join(left_j) -> L3_agg
+
+annotation_colors <- unique(L3_agg$Category)
+names(annotation_colors) <- unique(L3_agg$Category)
+annotation_colors <- list(Category = annotation_colors)
+
+my_gene_col <- data.frame(row.names = L3_agg$SuperPathway, 
+                          Category = factor(L3_agg$Category, levels = unique(L3_agg$Category)))
+
+my_sample_col <- mtd[-1] %>% as.data.frame()
+row.names(my_sample_col) <- colNames
+
+
+# 
+# n <- nrow(obj)
+# pal <- colorRampPalette(ggsci::pal_material(palette = c("purple"),n = n, alpha = 1, reverse = F)(n))
+
+L3_agg %>%
+  select_at(vars(colNames)) %>%
+  data.frame(row.names = rownames(my_gene_col), .) %>% 
+  pheatmap(., 
+           annotation_row = my_gene_col, 
+           annotation_col = my_sample_col,
+           cluster_rows = T,
+           # color = pal,
+           cluster_cols = T,
+           fontsize_row = 7,
+           show_rownames = T) -> my_pheatmap
+           # annotation_colors = annotation_colors) 
+
+save_pheatmap_png <- function(x, filename, width= 1500, height=1400, res = 150) {
+  png(filename, width = width, height = height, res = res)
+  grid::grid.newpage()
+  grid::grid.draw(x$gtable)
+  dev.off()
+}
+
+save_pheatmap_png(my_pheatmap,
+                  paste0(dir, "/pheatmap_kegg.png"))
